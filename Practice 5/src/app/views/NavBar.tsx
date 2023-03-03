1030cd4af29d123c10bfda006d6dd20d737de5d4
Muhamamad Wasif Arslan <66217748+muhammadwasifarslan@users.noreply.github.com>
2023-03-03 22:01:10 +0500

    changes

---
+++ Practice 5/src/app/views/NavBar.tsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+  return (
+    <div className="grid grid-cols-3">
+      <div>Logo</div>
+      <div>Home</div>
+      <div>Courses</div>
+      <div>SignUp</div>
+      <div className="grid grid-cols-4 gap-4">
+        <div>01</div>
+
+        <div>09</div>
+      </div>
+    </div>
+  );
+}
